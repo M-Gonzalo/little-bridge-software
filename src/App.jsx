0769ff7c4628263a49691c7e1838a5ba66b20d70
vadith gomez <legrone@MacBook-Pro.local>
2022-03-18 18:21:24 -0500

    header-footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import './App.css'
 import { BsLinkedin } from 'react-icons/bs'
 import { FaGithub } from 'react-icons/fa'
 
-// import Section1_Header from 'components/principal/Section1_Header/Section1_Header.jsx'
+import Section1_Header from './components/principal/Section1_Header/Section1_Header'
+
+
 // import Section2_AboutUs from 'components/principal/Section2_AboutUs/Section2_AboutUs.jsx'
 // import Section3_OurWork from 'components/principal/Section3_OurWork/Section3_OurWork.jsx'
 // import Section4_Contact from 'components/principal/Section4_Contact/Section4_Contact.jsx'
@@ -14,20 +16,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <div className='container'>
-          <h5>Element.Corp</h5>
-          <h1>Page Services</h1>
-          <div className='socials'>
-            <a href='https://www.linkedin.com/in/vadith-gomez-21808a223/' target='_blank' rel='noreferrer'><BsLinkedin /> </a>
-            <a href='https://github.com/lvadithe' target='_blank' rel='noreferrer'><FaGithub /> </a>
-          </div>
-          <div className='socials'>
-            <a href='https://www.linkedin.com/in/gonzalo-muñoz-arg/' target='_blank' rel='noreferrer'><BsLinkedin /> </a>
-            <a href='https://github.com/M-Gonzalo' target='_blank' rel='noreferrer'><FaGithub /> </a>
-          </div>
-        </div>
-      </header >
+      <Section1_Header />
     </div >
   )
 }
---
+++ src/components/principal/Section1_Header/Section1_Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Section1_Header.module.css'
+
+function Section1_Header() {
+    return (
+        <header id='header'>
+            <div className="container header_container">
+                <h1>Nebulae Software Solutions</h1>
+                <h3>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Nisi, fugiat facere impedit ad ullam quia aspernatur nobis
+                    optio deleniti nostrum quas recusandae dolor quaerat porro,
+                    non blanditiis explicabo? Distinctio, aliquid.
+                </h3>
+            </div>
+        </header>
+    )
+}
+
+export default Section1_Header
